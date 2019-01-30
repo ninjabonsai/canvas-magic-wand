482843d7bef50607c6c08f28806140cafb240118
ninjabonsai <rar@ninjabonsai.net>
2019-01-30 17:09:40 +0100

    added CanvasMagicWand module v1

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # canvas-magic-wand
+set a color within an image to be transparent
+## Installation and usage
+```
+npm install canvas-magic-wand
+```
+Then use it like this...
+```javascript
+import CanvasMagicWand from './canvas-magic-wand';
+
+const img = document.body.querySelector( 'img' );
+img.addEventListener( 'load', () => {
+    // CanvasMagicWand.knockoutColor(
+    //     img,
+    //     targetColor = { r: 255, g: 255, b: 255 },
+    //     tolerance = 10,
+    //     edgeBlur = 4,
+    //     edgeBrightness = 2,
+    //     edgeContrast = 6
+    // )
+    img.src = CanvasMagicWand.knockoutColor( img, { r: 255, g: 255, b: 255 }, 10, 4, 2, 6 ).toDataURL();
+}, { once: true } );
+```
+### Demo
+https://codepen.io/ninjabonsai/pen/PVbppm
---
+++ src/index.js
@@ -0,0 +1,92 @@
+function setImageData( imageData, targetColor, newColorSelected, newColorUnselected, tolerance ) {
+    const currentPixelColor = { r: null, g: null, b: null };
+    const pixels = imageData.data;
+    
+    const isMask = (
+        Math.abs( newColorSelected.r - newColorUnselected.r ) +
+        Math.abs( newColorSelected.g - newColorUnselected.g ) +
+        Math.abs( newColorSelected.b - newColorUnselected.b )
+    ) / 3 === 255;
+    
+    // loop through imageData
+    for ( let i = 0, n = imageData.data.length; i < n; i += 4 ) {
+        currentPixelColor.r = imageData.data[ i ];
+        currentPixelColor.g = imageData.data[ i + 1 ];
+        currentPixelColor.b = imageData.data[ i + 2 ];
+        
+        const diff = (
+            Math.abs( currentPixelColor.r - targetColor.r ) +
+            Math.abs( currentPixelColor.g - targetColor.g ) +
+            Math.abs( currentPixelColor.b - targetColor.b )
+        ) / 3;
+
+        if ( diff > tolerance ) {
+            pixels[ i ] = newColorUnselected.r;
+            pixels[ i + 1 ] = newColorUnselected.g;
+            pixels[ i + 2 ] = newColorUnselected.b;
+            pixels[ i + 3 ] = newColorSelected.a === 0 && isMask ? diff : newColorUnselected.a;
+        } else {
+            pixels[ i ] = newColorSelected.r;
+            pixels[ i + 1 ] = newColorSelected.g;
+            pixels[ i + 2 ] = newColorSelected.b;
+            pixels[ i + 3 ] = newColorSelected.a;
+        }
+    }
+}
+
+const CanvasMagicWand = {
+    knockoutColor (
+        img,
+        targetColor = { r: 255, g: 255, b: 255 },
+        tolerance = 10,
+        edgeBlur = 4,
+        edgeBrightness = 2,
+        edgeContrast = 6
+    ) {
+        const w = img.naturalWidth;
+        const h = img.naturalHeight;
+        
+        const newColorSelected = { r: 255, g: 255, b: 255, a: 255 };
+        const newColorUnselected = { r: 0, g: 0, b: 0, a: 255 };
+
+        const canvas1 = document.createElement( 'canvas' );
+        canvas1.width = w;
+        canvas1.height = h;
+
+        let ctx = canvas1.getContext( '2d' );
+        ctx.drawImage( img, 0, 0 );
+
+        let imageData = ctx.getImageData( 0, 0, w, h );
+
+        setImageData( imageData, targetColor, newColorSelected, newColorUnselected, tolerance );
+        ctx.putImageData( imageData, 0, 0 );
+
+        const canvas2 = document.createElement( 'canvas' );
+        canvas2.width = canvas1.width;
+        canvas2.height = canvas1.height;
+        ctx = canvas2.getContext( '2d' );
+        ctx.filter = `blur( ${ edgeBlur }px ) brightness( ${ edgeBrightness } ) contrast( ${ edgeContrast } )`;
+        ctx.drawImage( canvas1, 0, 0 );
+        
+        const canvas3 = document.createElement( 'canvas' );
+        canvas3.width = canvas1.width;
+        canvas3.height = canvas1.height;
+        ctx = canvas3.getContext( '2d' );
+        ctx.drawImage( canvas2, 0, 0 );
+        imageData = ctx.getImageData( 0, 0, w, h );
+        setImageData( imageData, { r:255, g:255, b:255 }, { ...newColorSelected, a: 0 }, newColorUnselected, 10 );
+        ctx.putImageData( imageData, 0, 0 );
+
+        const canvasDisplay = document.createElement( 'canvas' );
+        canvasDisplay.width = w;
+        canvasDisplay.height = h;
+        ctx = canvasDisplay.getContext( '2d' );
+        ctx.drawImage( canvas3, 0, 0 );
+        ctx.globalCompositeOperation = 'source-in';
+        ctx.drawImage( img, 0, 0 );
+        
+        return canvasDisplay;
+    }
+};
+
+export default CanvasMagicWand;
